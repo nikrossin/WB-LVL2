4019a6fdb2c1df41444257013800c69e9905e641
nikrossin <bns17u144@student.bmstu.ru>
2022-03-13 21:21:10 +0300

    add task

--- develop/dev08/internal/Shell.go
+++ develop/dev08/internal/Shell.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/mitchellh/go-ps"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"strconv"
@@ -29,94 +30,129 @@ func NewShell(out io.Writer, in io.Reader) *Shell {
 	return &Shell{out: out, in: in}
 }
 
+// задаем цвета вывода в Shell
 func (s *Shell) SetColors(system, pathDir, input string) {
 	s.colorSystemUser = system
 	s.colorDir = pathDir
 	s.colorInput = input
 }
 
+// задаем имя системы и пользователя для отображения в Shell
 func (s *Shell) SetSystem(sys, usr string) {
 	s.nameSystem = sys
 	s.nameUser = usr
 }
 
 func (s *Shell) Run() {
-	s.ScanLines()
+	if err := s.ScanLines(); err != nil {
+		if _, err := fmt.Fprintln(s.out, err); err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
+
+//вывод перед строкой ввода директории в которой находимся и пользователя
 func (s *Shell) PrefixComm() error {
 	if dir, err := os.Getwd(); err != nil {
 		return err
 	} else {
-		fmt.Fprintf(s.out, "%v%v@%v:%v%v%v$ ", s.colorSystemUser, s.nameUser, s.nameSystem, s.colorDir, dir, s.colorInput)
+		_, err = fmt.Fprintf(s.out, "%v%v@%v:%v%v%v$ ", s.colorSystemUser, s.nameUser, s.nameSystem, s.colorDir, dir, s.colorInput)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 	return nil
 }
 
-func (s *Shell) ScanLines() {
+// читаем команды до \quit
+func (s *Shell) ScanLines() error {
 	sc := bufio.NewScanner(s.in)
-	s.PrefixComm()
+	if err := s.PrefixComm(); err != nil {
+		return err
+	}
 	for sc.Scan() && sc.Text() != "\\quit" {
 		line := sc.Text()
-		s.CheckFork(line)
-		s.PrefixComm()
+		if err := s.CheckFork(line); err != nil {
+			return err
+		}
+		if err := s.PrefixComm(); err != nil {
+			return err
+		}
 	}
 
 	if sc.Err() != nil {
 		os.Exit(1)
 	}
+	return nil
 }
-func (s *Shell) CheckPipes(line string) {
 
+// Обработка строки на наличие конвеера
+func (s *Shell) CheckPipes(line string) error {
 	lineCmd := strings.Split(line, "|")
-	if len(lineCmd) > 1 {
-		s.buffPipe = new(bytes.Buffer)
+	if len(lineCmd) > 1 { // если конвеер
+		s.buffPipe = new(bytes.Buffer) //буфер между пайпами
 		s.isPipe = true
 		for index, comm := range lineCmd {
-			if index != 0 {
+			if index != 0 { // если не первая команда то разбиваем ее ( для args)
 				commSl := strings.Fields(comm)
-				if len(commSl) > 1 {
+				if len(commSl) > 1 { // если args есть у команды, то заменяем их на значения из буфера
 					commSlNew := make([]string, 2, 2)
 					commSlNew[0], commSlNew[1] = commSl[0], s.buffPipe.String()
 					commSl = commSlNew
 				} else {
-					commSl = append(commSl, s.buffPipe.String())
+					commSl = append(commSl, s.buffPipe.String()) // просто добавление args из буфера
 				}
 				comm = strings.Join(commSl, " ")
 			}
-			if index == len(lineCmd)-1 {
+			if index == len(lineCmd)-1 { // если команда последняя - вывод в stdout, а не в буффер
 				s.isPipe = false
 				s.buffPipe.Reset()
 			}
 			if err := s.CaseCommand(comm); err != nil {
 
-				fmt.Fprintln(s.out, err)
+				if _, err = fmt.Fprintln(s.out, err); err != nil {
+					return err
+				}
 			}
 
 		}
 
-	} else {
+	} else { // если нет конвеера
 		if err := s.CaseCommand(line); err != nil {
-			fmt.Fprintln(s.out, err)
+			if _, err = fmt.Fprintln(s.out, err); err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
 
-func (s *Shell) CheckFork(line string) {
+//Обработка строки на наличие fork()
+func (s *Shell) CheckFork(line string) error {
 	line = strings.TrimRight(line, " ")
 	if strings.Contains(line, "&") {
 		if id, _, err := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0); err != 0 {
 			os.Exit(1)
 		} else if id == 0 {
 			line = strings.TrimSuffix(line, "&")
-			fmt.Fprintf(s.out, "[%v]\n", os.Getpid())
-			s.CheckPipes(line)
-			fmt.Fprintf(s.out, "[%v] Завершен\n", os.Getpid())
+			if _, err := fmt.Fprintf(s.out, "[%v]\n", os.Getpid()); err != nil {
+				return err
+			}
+			if err := s.CheckPipes(line); err != nil {
+				return err
+			}
+			if _, err := fmt.Fprintf(s.out, "[%v] Завершен\n", os.Getpid()); err != nil {
+				return err
+			}
 			os.Exit(0)
 		}
 
 	} else {
-		s.CheckPipes(line)
+		if err := s.CheckPipes(line); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func (s *Shell) cd(param string) error {
@@ -131,9 +167,13 @@ func (s *Shell) pwd() error {
 		return err
 	} else {
 		if s.isPipe {
-			fmt.Fprintln(s.buffPipe, dir)
+			if _, err := fmt.Fprintln(s.buffPipe, dir); err != nil {
+				return err
+			}
 		} else {
-			fmt.Fprintln(s.out, dir)
+			if _, err := fmt.Fprintln(s.out, dir); err != nil {
+				return err
+			}
 		}
 
 	}
@@ -142,11 +182,11 @@ func (s *Shell) pwd() error {
 
 func (s *Shell) kill(procIds []string) []error {
 	var errs []error
-	for _, pid := range procIds {
+	for _, pid := range procIds { // может быть много PID у команды
 		if id, err := strconv.Atoi(pid); err != nil {
 			err = fmt.Errorf("%v pid: %v", err, id)
 		} else {
-			if errKill := syscall.Kill(id, syscall.SIGTERM); errKill != nil {
+			if errKill := syscall.Kill(id, syscall.SIGTERM); errKill != nil { // по умолчанию в Шелл сигнал SIGTERM
 				errKill = fmt.Errorf("%v pid: %v", errKill, id)
 				errs = append(errs, errKill)
 			}
@@ -158,7 +198,7 @@ func (s *Shell) kill(procIds []string) []error {
 func (s *Shell) echo(args []string, fullComm string) error {
 	strStart := args[0]
 	strEnd := args[len(args)-1]
-	if strStart[0] == '"' && strEnd[len(strEnd)-1] == '"' {
+	if strStart[0] == '"' && strEnd[len(strEnd)-1] == '"' { // поддержка вывода фиксированной строки
 		start, end := strings.Index(fullComm, "\""), strings.LastIndex(fullComm, "\"")
 		var newArgs []string
 		newArgs = append(newArgs, fullComm[start+1:end])
@@ -182,14 +222,19 @@ func (s *Shell) ps() error {
 		if s.isPipe {
 			output = s.buffPipe
 		}
-		fmt.Fprintf(output, "%50v%10v%10v\n", "CMD", "PID", "PPI")
+		if _, err := fmt.Fprintf(output, "%50v%10v%10v\n", "CMD", "PID", "PPI"); err != nil {
+			return err
+		}
 		for _, proc := range procs {
-			fmt.Fprintf(output, "%50v%10v%10v\n", proc.Executable(), proc.Pid(), proc.PPid())
+			if _, err := fmt.Fprintf(output, "%50v%10v%10v\n", proc.Executable(), proc.Pid(), proc.PPid()); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
+// выполнение внешних команд
 func (s *Shell) exec(args []string) error {
 	var command *exec.Cmd
 	if len(args) == 1 {
@@ -214,7 +259,9 @@ func (s *Shell) CaseCommand(comm string) error {
 		case "cd":
 			if len(setComm) == 2 {
 				if err := s.cd(setComm[1]); err != nil {
-					fmt.Println(err)
+					if _, err := fmt.Fprintln(s.out, err); err != nil {
+						return err
+					}
 				}
 			} else {
 				return errChangeDir
@@ -231,7 +278,9 @@ func (s *Shell) CaseCommand(comm string) error {
 			if len(setComm) >= 2 {
 				errs := s.kill(setComm[1:])
 				for _, err := range errs {
-					fmt.Fprintf(s.out, "%v ", err)
+					if _, err := fmt.Fprintf(s.out, "%v ", err); err != nil {
+						return err
+					}
 				}
 			} else {
 				return errKill
